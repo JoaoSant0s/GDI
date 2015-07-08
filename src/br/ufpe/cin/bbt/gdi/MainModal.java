66495953c0bba2a8acc360ab0d1f708255c9d55e
jrs4 <jrs4@cin.ufpe.br>
2015-07-08 12:06:42 -0300

    mais alterações na interface

--- src/br/ufpe/cin/bbt/gdi/MainModal.java
+++ src/br/ufpe/cin/bbt/gdi/MainModal.java
@@ -1,6 +1,7 @@
 package br.ufpe.cin.bbt.gdi;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import Entities.Cliente;
 import Entities.Endereco;
@@ -23,6 +24,7 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
@@ -37,6 +39,7 @@ import javafx.stage.Stage;
 
 
 public class MainModal extends Application{
+	//Campos para cadastro do Cliente
 	final Label lblNomeCliente = new Label("Nome do Cliente");
 	final TextField dialogoNomeCliente = new TextField();
 	
@@ -52,25 +55,43 @@ public class MainModal extends Application{
 	final Label lblTipoEmprego = new Label("Tipo Emprego");
 	final TextField dialogoTipoEmprego = new TextField();
 	
-	final Label lblEnderecoEstado = new Label("Estado");
-	final TextField dialogoEnderecoEstado = new TextField();
+	final Label lblEnderecoEstadoCliente = new Label("Estado");
+	final TextField dialogoEnderecoEstadoCliente = new TextField();
 	
-	final Label lblEnderecoCidade = new Label("Cidade");
-	final TextField dialogoEnderecoCidade = new TextField();
+	final Label lblEnderecoCidadeCliente = new Label("Cidade");
+	final TextField dialogoEnderecoCidadeCliente = new TextField();
 
-	final Label lblEnderecoBairro = new Label("Bairro");
-	final TextField dialogoEnderecoBairro = new TextField();
+	final Label lblEnderecoBairroCliente = new Label("Bairro");
+	final TextField dialogoEnderecoBairroCliente = new TextField();
 	
-	final Label lblEnderecoRua = new Label("Rua");
-	final TextField dialogoEnderecoRua = new TextField();
+	final Label lblEnderecoRuaCliente = new Label("Rua");
+	final TextField dialogoEnderecoRuaCliente = new TextField();
 	
-	final Label lblEnderecoNumero = new Label("Numero");
-	final TextField dialogoEnderecoNumero = new TextField();
+	final Label lblEnderecoNumeroCliente = new Label("Numero");
+	final TextField dialogoEnderecoNumeroCliente = new TextField();
 	
-	final Label lblEnderecoComplemento = new Label("Complemento");
-	final TextField dialogoEnderecoComplemento = new TextField();	
+	final Label lblEnderecoComplementoCliente = new Label("Complemento");
+	final TextField dialogoEnderecoComplementoCliente = new TextField();	
+	
+	//Dados para mostragem na busca cliente
+	final Label lblBuscaNomeCliente = new Label("Busca por Nome");
+	final TextField dialogoBuscaNomeCliente = new TextField();	
+	
+	final Label lblDadosNomeCliente = new Label();
+	final Label lblDadosCPFCliente = new Label();
+	final Label lblDadosEmailCliente = new Label();
+	final ImageView imageCliente = new ImageView();
+	final Label lblDadosTipoEmpregoCliente = new Label();
+	
+	final Label lblDadosEnderecoEstadoCliente = new Label();
+	final Label lblDadosEnderecoCidadeCliente = new Label();
+	final Label lblDadosEnderecoBairroCliente = new Label();
+	final Label lblDadosEnderecoRuaCliente = new Label();
+	final Label lblDadosEnderecoNumeroCliente = new Label();
+	final Label lblDadosEnderecoComplementoCliente = new Label();
 	
 	
+	//Campos para cadastro do Funcionario
 	final Label lblNomeFuncionario = new Label("Nome do Funcionário");
 	final TextField dialogoNomeFuncionario = new TextField();
 	
@@ -113,18 +134,38 @@ public class MainModal extends Application{
 	final Label lblEnderecoComplementoFuncionario = new Label("Complemento");
 	final TextField dialogoEnderecoComplementoFuncionario = new TextField();	
 	
+	//Dados para mostragem na busca Funcionario
+		final Label lblBuscaNomeFuncionario = new Label("Busca por Nome");
+		final TextField dialogoBuscaNomeFuncionario = new TextField();	
+		
+		final Label lblDadosNomeFuncionario = new Label();
+		final Label lblDadosCPFFuncionario= new Label();
+		final Label lblDadosEmailFuncionario = new Label();
+		final ImageView imageFuncionario = new ImageView();
+		final Label lblDadosTipoEmpregoFuncionario = new Label();
+		
+		final Label lblDadosEnderecoEstadoFuncionario = new Label();
+		final Label lblDadosEnderecoCidadeFuncionario = new Label();
+		final Label lblDadosEnderecoBairroFuncionario = new Label();
+		final Label lblDadosEnderecoRuaFuncionario = new Label();
+		final Label lblDadosEnderecoNumeroFuncionario = new Label();
+		final Label lblDadosEnderecoComplementoFuncionario = new Label();
+	
+	//Botoes para cadastros gerais
+	final Button btnCliente = new Button("Cadastrar Cliente");
+	final Button btnFuncionario = new Button("Cadastrar Funcionário");
+	final Button btnInvestidor = new Button("Cadastrar Investidor");
+	
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void start(Stage stage) throws Exception {
 		/*DBController controller = new DBController();
 		controller.createConnection();
-		RepositorioCliente repoCliente = new RepositorioCliente(controller.getConnection());
-		RepositorioFuncionario repoFuncionario = new RepositorioFuncionario(controller.getConnection());
-		*/
-			Button btnCliente = new Button("Cadastrar Cliente");
-			Button btnFuncionario = new Button("Cadastrar Funcionário");
-			Button btnInvestidor = new Button("Cadastrar Investidor");
+		final RepositorioCliente repoCliente = new RepositorioCliente(controller.getConnection());
+		RepositorioFuncionario repoFuncionario = new RepositorioFuncionario(controller.getConnection());*/
+		
+			
 						
 		 EventHandler<MouseEvent> filter1 = new EventHandler<MouseEvent>() {
 	            @Override
@@ -132,16 +173,26 @@ public class MainModal extends Application{
 	            	cadastrarCliente();
 	            }
 				private void cadastrarCliente() {
-					Imagem image = null;
-					/*try {
-						image = new Imagem(imageTexFunc);
+					/*Imagem image = null;
+					// TODO Como pegar imagem?
+					try {
+						image = new Imagem(dialogoImageCliente.getText());
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
-					}*/
-		            
-		            //Endereco novoEndereco = new Endereco(0, enderecoEstado, enderecoCidade, enderecoBairro, enderecoRua, enderecoNumeroCliente , enderecoComplemento);
+					}
+		            Endereco novoEndereco = new Endereco(0, dialogoEnderecoEstado.getText(), dialogoEnderecoCidade.getText(), dialogoEnderecoBairro.getText(), dialogoEnderecoRua.getText(), Integer.parseInt(dialogoEnderecoNumero.getText()) , dialogoEnderecoComplemento.getText());
 					
-					//Cliente novo = new Cliente(cpf, nomeCliente, email, image, novoEndereco, tipoEmprego);
+					Cliente novo = new Cliente(dialogoCPFCliente.getText(), dialogoNomeCliente.getText(), dialogoEmailCliente.getText(), image, novoEndereco, dialogoTipoEmprego.getText());
+					
+					try {
+						repoCliente.inserirCliente(novo);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}*/
 				}
 	        };
 	        
@@ -152,6 +203,7 @@ public class MainModal extends Application{
             }
 			private void cadastrarFuncionario() {
 				Imagem image = null;
+				
 				/*try {
 					image = new Imagem(imageTextCliente);
 				} catch (IOException e) {
@@ -229,10 +281,43 @@ public class MainModal extends Application{
 		        Tab tabFuncionarioBuscar = new Tab();
 		        tabFuncionarioBuscar.setText("Buscar");
 		        tabFuncionarioBuscar.closableProperty().set(false);
-			        HBox hbox2 = new HBox();
-			        //hbox2.getChildren().addAll(lblFuncionario, dialogoNomeFuncionario);
-		            hbox2.setAlignment(Pos.TOP_LEFT);
-		            tabFuncionarioBuscar.setContent(hbox2);
+		        GridPane grid2 = new GridPane();
+		        
+				grid2.setHgap(1);
+			    grid2.setVgap(1);
+			    
+			    grid2.add(lblBuscaNomeFuncionario, 20, 20);
+			    grid2.add(dialogoBuscaNomeFuncionario, 40, 20);
+		        
+		        int con = 0;
+			    //for (Cliente cliente : repoCliente.getClientes()) {
+				    lblDadosNomeFuncionario.setText("Nome Aqui");
+				    lblDadosCPFFuncionario.setText("CPF Aqui");
+				    lblDadosEmailFuncionario.setText("Email Aqui");
+				    lblDadosTipoEmpregoFuncionario.setText("Emprego Aqui");
+				    lblDadosEnderecoEstadoFuncionario.setText("Estado Aqui");
+				    lblDadosEnderecoCidadeFuncionario.setText("Cidade Aqui");
+				    lblDadosEnderecoBairroFuncionario.setText("Bairro Aqui");
+				    lblDadosEnderecoRuaFuncionario.setText("Rua Aqui");
+				    lblDadosEnderecoNumeroFuncionario.setText("Numero Aqui");
+				    lblDadosEnderecoComplementoFuncionario.setText("complemento Aqui");
+				    
+				    grid2.add(imageFuncionario, 40, 40 + con);
+				    grid2.add(lblDadosNomeFuncionario, 60, 40 + con);
+				    grid2.add(lblDadosCPFFuncionario, 80, 40 + con);
+				    grid2.add(lblDadosEmailFuncionario, 100, 40 + con);
+				    grid2.add(lblDadosTipoEmpregoFuncionario, 120, 40 + con);
+				    
+				    grid2.add(lblDadosEnderecoEstadoFuncionario, 40, 50 + con);
+				    grid2.add(lblDadosEnderecoCidadeFuncionario, 60, 50 + con);
+				    grid2.add(lblDadosEnderecoBairroFuncionario, 80, 50 + con);
+				    grid2.add(lblDadosEnderecoRuaFuncionario, 100, 50 + con);
+				    grid2.add(lblDadosEnderecoNumeroFuncionario, 120, 50 + con);
+				    grid2.add(lblDadosEnderecoComplementoFuncionario, 140, 50 + con);
+				    con = con + 30;
+				    //}
+			        
+		            tabFuncionarioBuscar.setContent(grid2);
 		        tabPanFuncionario.getTabs().add(tabFuncionarioBuscar);
 		        
 		    //END PAN_FUNCIONARIO_INTERNO
@@ -255,48 +340,79 @@ public class MainModal extends Application{
 			        Tab tabClienteCadastrar = new Tab();
 			        tabClienteCadastrar.setText("Cadastrar");
 			        tabClienteCadastrar.closableProperty().set(false);
-					GridPane grid2 = new GridPane();
+					GridPane grid3 = new GridPane();
 				        
-				       
-						
-						grid2.setHgap(1);
-					    grid2.setVgap(1);
+						grid3.setHgap(1);
+						grid3.setVgap(1);
 					    
-					    grid2.add(lblNomeCliente, 40, 70);
-					    grid2.add(dialogoNomeCliente, 65, 70);
-					    grid2.add(lblCPFCliente, 40, 100);
-					    grid2.add(dialogoCPFCliente, 65, 100);
-					    grid2.add(lblEmailCliente, 40, 130);
-					    grid2.add(dialogoEmailCliente, 65, 130);
-					    grid2.add(lblImageCliente, 40, 160);
-					    grid2.add(dialogoImageCliente, 65, 160);
-					    grid2.add(lblTipoEmprego, 40, 190);
-					    grid2.add(dialogoTipoEmprego, 65, 190);
-					    grid2.add(btnCliente, 80, 230);
+						grid3.add(lblNomeCliente, 40, 70);
+						grid3.add(dialogoNomeCliente, 65, 70);
+						grid3.add(lblCPFCliente, 40, 100);
+						grid3.add(dialogoCPFCliente, 65, 100);
+						grid3.add(lblEmailCliente, 40, 130);
+						grid3.add(dialogoEmailCliente, 65, 130);
+						grid3.add(lblImageCliente, 40, 160);
+						grid3.add(dialogoImageCliente, 65, 160);
+						grid3.add(lblTipoEmprego, 40, 190);
+					    grid3.add(dialogoTipoEmprego, 65, 190);
+					    grid3.add(btnCliente, 80, 230);
 					    
-					    grid2.add(lblEnderecoEstado, 110, 70);
-					    grid2.add(dialogoEnderecoEstado, 125, 70);
-					    grid2.add(lblEnderecoCidade, 110, 100);
-					    grid2.add(dialogoEnderecoCidade, 125, 100);
-					    grid2.add(lblEnderecoBairro, 110, 130);
-					    grid2.add(dialogoEnderecoBairro, 125, 130);
-					    grid2.add(lblEnderecoRua, 110, 160);
-					    grid2.add(dialogoEnderecoRua, 125, 160);
-					    grid2.add(lblEnderecoNumero, 110, 190);
-					    grid2.add(dialogoEnderecoNumero, 125, 190);
-					    grid2.add(lblEnderecoComplemento, 110, 210);
-					    grid2.add(dialogoEnderecoComplemento, 125, 210);
+					    grid3.add(lblEnderecoEstadoCliente, 110, 70);
+					    grid3.add(dialogoEnderecoEstadoCliente, 125, 70);
+					    grid3.add(lblEnderecoCidadeCliente, 110, 100);
+					    grid3.add(dialogoEnderecoCidadeCliente, 125, 100);
+					    grid3.add(lblEnderecoBairroCliente, 110, 130);
+					    grid3.add(dialogoEnderecoBairroCliente, 125, 130);
+					    grid3.add(lblEnderecoRuaCliente, 110, 160);
+					    grid3.add(dialogoEnderecoRuaCliente, 125, 160);
+					    grid3.add(lblEnderecoNumeroCliente, 110, 190);
+					    grid3.add(dialogoEnderecoNumeroCliente, 125, 190);
+					    grid3.add(lblEnderecoComplementoCliente, 110, 210);
+					    grid3.add(dialogoEnderecoComplementoCliente, 125, 210);
 					   
-			            tabClienteCadastrar.setContent(grid2);
+			            tabClienteCadastrar.setContent(grid3);
 			        tabPanCliente.getTabs().add(tabClienteCadastrar);
 			        
 			        Tab tabClienteBuscar = new Tab();
 			        tabClienteBuscar.setText("Buscar");
 			        tabClienteBuscar.closableProperty().set(false);
-				        HBox hbox4 = new HBox();
-				        //hbox4.getChildren().addAll(lblCliente, dialogoNomeCliente);
-			            hbox4.setAlignment(Pos.TOP_LEFT);
-			            tabClienteBuscar.setContent(hbox4);
+				        GridPane grid4 = new GridPane();
+				        
+				        grid4.setHgap(1);
+				        grid4.setVgap(1);
+					    
+				        grid4.add(lblBuscaNomeCliente, 20, 20);
+				        grid4.add(dialogoBuscaNomeCliente, 40, 20);
+					    
+					    con = 0;
+					    //for (Cliente cliente : repoCliente.getClientes()) {
+						    lblDadosNomeCliente.setText("Nome Aqui");
+						    lblDadosCPFCliente.setText("CPF Aqui");
+						    lblDadosEmailCliente.setText("Email Aqui");
+						    lblDadosTipoEmpregoCliente.setText("Emprego Aqui");
+						    lblDadosEnderecoEstadoCliente.setText("Estado Aqui");
+						    lblDadosEnderecoCidadeCliente.setText("Cidade Aqui");
+						    lblDadosEnderecoBairroCliente.setText("Bairro Aqui");
+						    lblDadosEnderecoRuaCliente.setText("Rua Aqui");
+						    lblDadosEnderecoNumeroCliente.setText("Numero Aqui");
+						    lblDadosEnderecoComplementoCliente.setText("complemento Aqui");
+						    
+						    grid4.add(imageCliente, 40, 40 + con);
+						    grid4.add(lblDadosNomeCliente, 60, 40 + con);
+						    grid4.add(lblDadosCPFCliente, 80, 40 + con);
+						    grid4.add(lblDadosEmailCliente, 100, 40 + con);
+						    grid4.add(lblDadosTipoEmpregoCliente, 120, 40 + con);
+						    
+						    grid4.add(lblDadosEnderecoEstadoCliente, 40, 50 + con);
+						    grid4.add(lblDadosEnderecoCidadeCliente, 60, 50 + con);
+						    grid4.add(lblDadosEnderecoBairroCliente, 80, 50 + con);
+						    grid4.add(lblDadosEnderecoRuaCliente, 100, 50 + con);
+						    grid4.add(lblDadosEnderecoNumeroCliente, 120, 50 + con);
+						    grid4.add(lblDadosEnderecoComplementoCliente, 140, 50 + con);
+						    con = con + 30;
+						//}
+					    
+			        tabClienteBuscar.setContent(grid4);
 			        tabPanCliente.getTabs().add(tabClienteBuscar);
 		    //END PAN_CLIENTE_INTERNO
 		        
